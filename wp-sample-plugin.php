29b5c948ddd53236ccdb6be4d1f958382bd523b4
niiharamegumu <nhrmgm1223@gmail.com>
2018-10-16 21:03:53 +0900

    Add standard plugin information.

---
+++ wp-sample-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: WordPress Sample Plugin
+Plugin URI: https://github.com/niiharamegumu/wp-sample-plugin
+Description: WordPress Plugin Sample build.
+Version: 1.0.0
+Author: Niihara Megumu
+Author URI: https://github.com/niiharamegumu/wp-sample-plugin
+License: GPLv2 or later
+*/
