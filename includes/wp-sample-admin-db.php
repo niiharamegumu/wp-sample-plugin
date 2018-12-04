fbf922386eedb2c30c393d38f87ada79802f0305
niiharamegumu <nhrmgm1223@gmail.com>
2018-12-04 20:59:07 +0900

    2018.12.4 commit.

--- includes/wp-sample-admin-db.php
+++ includes/wp-sample-admin-db.php
@@ -55,6 +55,21 @@ Class Sample_Plugin_Admin_Db {
 		}
 	}
 	/**
+	* Set data.
+	*
+	* @version 1.0.0
+	* @since 1.0.0
+	*/
+	public function get_list_options () {
+		global $wpdb;
+		$prepared = 'SELECT * FROM ' . $this->table_name . ' ORDER BY update_date DESC';
+		return $wpdb->get_results( $prepared );
+	}
+	
+	
+	
+	
+	/**
 	* Insert Post.
 	*
 	* @version 1.0.0
@@ -62,7 +77,34 @@ Class Sample_Plugin_Admin_Db {
 	* @param array $post
 	*/
 	public function insert_options ( array $post ) {
-		var_dump( $post );
+		global $wpdb;
+		
+		$data = array(
+			'image_url'            => $post['sample-image-url'],
+			'image_alt'            => $post['sample-image-alt'],
+			'link_url'             => $post['sample-image-link'],
+			'open_new_tab'         => isset($post['sample-image-target'] ) ? 1 : 0,
+			'insert_element_class' => $post['sample-element-class'],
+			'insert_element_id'    => $post['sample-element-id'],
+			'how_display'          => $post['sample-how-display'],
+			'filter_category'      => isset($post['sample-filter-category'] ) ? 1 : 0,
+			'category_id'          => $post['sample-display-category'],
+			'register_date'        => date('Y-m-d H:i:s'),
+			'update_date'          => date('Y-m-d H:i:s')
+		);
+		$prepared = array(
+			'%s',
+			'%s',
+			'%s',
+			'%d',
+			'%s',
+			'%s',
+			'%s',
+			'%d',
+			'%d'
+		);
+		$wpdb->insert( $this->table_name, $data, $prepared );	
+		return $wpdb->insert_id;
 	}
 	
 }
