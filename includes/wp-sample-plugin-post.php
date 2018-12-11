cb1d0b2b55643947a7d64241f8d64648a8d31b4a
niiharamegumu <nhrmgm1223@gmail.com>
2018-12-11 21:02:50 +0900

    2018.12.11 commit.

--- includes/wp-sample-plugin-post.php
+++ includes/wp-sample-plugin-post.php
@@ -46,7 +46,14 @@ Class Sample_Plugin_Post {
 		$html .= '<tr>';
 		$html .= '<th>画像のURL（必須）</th>';
 		$html .= '<td>';
-		$html .= '<img id="banner-image-view" src="' . plugins_url('../images/no-image.png', __FILE__) . '" width="200">';
+		
+		if ( isset( $args->image_url ) ) {
+			$image_src = $args->image_url;	
+		} else {
+			$image_src = plugins_url('../images/no-image.png', __FILE__);
+		}
+		
+		$html .= '<img id="banner-image-view" src="' . $image_src . '" width="200">';
 		$html .= '<input id="banner-image-url" type="text" name="sample-image-url" class="large-text" required value="' . $args->image_url . '">';
 		$html .= '<button id="media-upload" type="botton" class="button">画像を選択</button>';
 		$html .= '</td>';
@@ -68,7 +75,14 @@ Class Sample_Plugin_Post {
 		
 		$html .= '<tr>';
 		$html .= '<th>新規タブを開く</th>';
-		$html .= '<td><input type="checkbox" name="sample-image-target" value="' . $args->open_new_tab . '">リンクを新規タブで開く</td>';
+		
+		if ( $args->open_new_tab === "1" ) {
+			$open_new_tab_checked = ' checked';
+		} else {
+			$open_new_tab_checked = '';
+		}
+		
+		$html .= '<td><input type="checkbox" name="sample-image-target" value="1"' . $open_new_tab_checked . '>リンクを新規タブで開く</td>';
 		$html .= '</tr>';
 		
 		$html .= '<tr>';
@@ -96,15 +110,35 @@ Class Sample_Plugin_Post {
 		$html .= '<tr>';
 		$html .= '<th>表示方法（必須）</th>';
 		$html .= '<td>';
-		$html .= '<input type="radio" name="sample-how-display" value="post_bottom">記事の下に表示<br>';
-		$html .= '<input type="radio" name="sample-how-display" value="shortcode">ショートコードで表示';
+		
+		$how_display_checked = array( '', '' );
+		switch ( $args->how_display ) {
+			case 'post_bottom': 
+				$how_display_checked[0] = ' checked';
+				break;
+			case 'shortcode': 
+				$how_display_checked[1] = ' checked';
+				break;
+			default:
+				break;
+		}
+		
+		$html .= '<input type="radio" name="sample-how-display" value="post_bottom"' . $how_display_checked[0] . '>記事の下に表示<br>';
+		$html .= '<input type="radio" name="sample-how-display" value="shortcode"' . $how_display_checked[1] . '>ショートコードで表示';
 		$html .= '</td>';
 		$html .= '</tr>';
 		
 		$html .= '<tr>';
 		$html .= '<th>絞り込み</th>';
 		$html .= '<td>';
-		$html .= '<input type="checkbox" name="sample-filter-category" value="' . $args->filter_category . '">テゴリーで絞り込み';
+		
+		if ( $args->filter_category === '1' ) {
+			$filter_category_checked = ' checked';
+		} else {
+			$filter_category_checked = '';
+		}
+		
+		$html .= '<input type="checkbox" name="sample-filter-category" value="1"' . $filter_category_checked . '>テゴリーで絞り込み';
 		$html .= '<p class="description">チェックされていない場合は、すべてに無条件で表示され、「表示するカテゴリ」項目の設定は無視されます。</p>';
 		$html .= '</td>';
 		$html .= '</tr>';
@@ -115,11 +149,12 @@ Class Sample_Plugin_Post {
 		
 		echo $html;
 		
-		$args = array(
+		$param = array(
 			'name'         => 'sample-display-category',
 			'hierarchical' => 1,
+			'selected'     => $args->category_id
 		);
-		wp_dropdown_categories( $args );
+		wp_dropdown_categories( $param );
 		
 		$html  = '<p class="description">選択したカテゴリーが投稿に紐づいている場合のみ画像が表示されます。</p>';
 		$html .= '</td>';
